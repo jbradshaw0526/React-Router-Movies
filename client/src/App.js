11ad7b6e3cfe6e0405b6e4a8585de91dc53de7d7
Your Name <jbradshaw0526@gmail.com>
2021-09-13 23:15:00 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Link, Switch } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
-export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+export default function App()
+{
+    const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+    const [movieList, setMovieList] = useState(null);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+    useEffect(() =>
+    {
+        const getMovies = () =>
+        {
+            axios
+                .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+                .then(response =>
+                {
+                    // Study this response with a breakpoint or log statements
+                    // and set the response data as the 'movieList' slice of state
+                    setMovieList(response.data);
+                })
+                .catch(error =>
+                {
+                    console.error('Server Error', error);
+                });
+        };
+        getMovies();
+    }, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+    const addToSavedList = id =>
+    {
+        // This is stretch. Prevent the same movie from being "saved" more than once
+    };
 
-  return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
+    return (
+        <div>
+            <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+            <nav>
+                <div className='nav-links'>
+                    <Link to="/">Home</Link>
+                    <Link to="/movies">MovieList</Link>
+                </div>
+            </nav>
+
+            <Switch>
+                <Route path={"/movies/:id"}>
+                    <Movie movies={movieList} />
+                </Route>
+
+                <Route path="/movies">
+                    {movieList && <MovieList movies={movieList} />}
+                </Route>
+                <Route path="/"></Route>
+            </Switch>
+        </div>
+    );
 }
